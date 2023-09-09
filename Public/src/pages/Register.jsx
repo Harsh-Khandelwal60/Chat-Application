95777a78c9d614440b4a3c73d901b7c80008d1de
Harsh Khandelwal <khandelwalharsh121@gmail.com>
2023-09-09 21:17:17 +0530

    second Commit

--- Public/src/pages/Register.jsx
+++ Public/src/pages/Register.jsx
@@ -1,18 +1,60 @@
-import React from "react";
+import React , {useState , useEffect} from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import styled from  "styled-components";
+import "react-toastify/dist/ReactToastify.css";
 import Logo from "../assets/logo.svg";
 
 const Register = () => {
 
+  const [values , setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  })
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error( "Password and confirm password should be same.",toastOptions);
+      return false;
+    
+    } else if (username.length < 3) {
+      toast.error("Username should be greater than 3 characters.",toastOptions);
+      return false;
+    } else if (password.length < 8) {
+      toast.error("Password should be equal or greater than 8 characters.",toastOptions);
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+
+    return true;
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("form")
+    handleValidation()
   }
-  const handleChange = (event) => {}
+  const handleChange = (event) => {
+    setValues({
+    ...values,
+      [event.target.name]: event.target.value
+    });
+  }
 
   return (
-    <FormContainer>
+    <>
+      <FormContainer>
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <img src={Logo} alt="logo" />
@@ -48,8 +90,11 @@ const Register = () => {
           </span>
         </form>
       </FormContainer>
+      <ToastContainer/>
+    </>
   )
 }
+
 
 const FormContainer = styled.div`
 height: 100vh;
